97de9e42dcd9694957f4ad08cd2fd259590a22f7
Vinicius Rezende <viniciusarantes133@gmail.com>
2023-08-11 09:02:39 -0300

    Project Completed

---
+++ Notas/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Notas
+//
+//  Created by Vinicius Rezende on 05/01/23.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad(
+        
+        
+    }
+
+
+}
+
